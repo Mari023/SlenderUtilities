d2521832b0c4c2e9ae3c6a98fa7104df62215c2f
Mari023 <Mari_023@outlook.de>
2019-10-03 00:06:00 +0200

    changed version to 0.1.2
    just a cosmetic change, the content was already out there

--- src/main/java/de/Mari_023/slenderutilities/util/Reference.java
+++ src/main/java/de/Mari_023/slenderutilities/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "slenderutilities";
 	public static final String NAME = "Slenderutilities";
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.1.2";
 	public static final String ACEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "de.Mari_023.slenderutilities.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "de.Mari_023.slenderutilities.proxy.CommonProxy";
